d0f0b4153d58e5785de49030fac8f58e63e45674
therealjustindude <therealjustindude@gmail.com>
2021-02-16 22:06:48 -0600

    FlatList displaying touchableOpacity list

--- weBudget/src/screens/Expenses.js
+++ weBudget/src/screens/Expenses.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import React, { useRef} from 'react';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
 
-export default function Expenses() {
+let data=[
+	{id: 0,paid: 'No', date: '02/21/2021', title: 'Medical Bill', amount:'$50'},
+	{id: 1,paid: 'Yes', date: '02/21/2021', title: 'Gas', amount:'$20'},
+	{id: 2,paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
+	{id: 3,paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
+	{id: 4,paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
+	{id: 5,paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
+]
+
+const Expenses = () => {
 	return (
 	<SafeAreaView >
 	  <View >
@@ -11,50 +20,43 @@ export default function Expenses() {
 		  <Text style={styles.headText}>Title</Text>
 		  <Text style={styles.headText}>Amount</Text>
 		</View>
-		<ScrollView>
-		{data.map(row => {
-		  return (
-			<View style={styles.rowCard}>
-			  <Text>{row.paid}</Text>
-			  <Text>{row.date}</Text>
-			  <Text>{row.title}</Text>
-			  <Text>{row.amount}</Text>
-			</View>
-		  )
-		})}
-		</ScrollView>
+		<FlatList
+			data={data}
+			renderItem={({ item }) => {
+				return(<TouchableOpacity style={styles.rowCard}>
+					<Text>{item.paid}</Text>
+					<Text>{item.date}</Text>
+					<Text>{item.title}</Text>
+					<Text>{item.amount}</Text>
+				</TouchableOpacity>	)
+			}}
+			keyExtractor={item => item.id}
+		/>
 	  </View>
 	</SafeAreaView>
 	);
-  }
+}
+export default Expenses;
   
-  let data=[
-	{paid: 'No', date: '02/21/2021', title: 'Medical Bill', amount:'$50'},
-	{paid: 'Yes', date: '02/21/2021', title: 'Gas', amount:'$20'},
-	{paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
-	{paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
-	{paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
-	{paid: 'No', date: '02/21/2021', title: 'Car Bill', amount:'$350'},
-  ]
-  
-  const styles = StyleSheet.create({
-	head: {
-	  flexDirection: 'row',
-	  justifyContent: 'space-around'
-	},
-	headText: {
-	  fontSize: 15,
-	  fontWeight: 'bold'
-	},
-	rowCard: {
-	  height: 50,
-	  borderWidth: 2,
-	  borderBottomLeftRadius: 10,
-	  borderTopRightRadius: 10,
-	  margin: 5,
-	  flexDirection: 'row',
-	  alignItems: 'center',
-	  justifyContent: 'space-around'
-	},
+const styles = StyleSheet.create({
+head: {
+	flexDirection: 'row',
+	justifyContent: 'space-around'
+},
+headText: {
+	fontSize: 15,
+	fontWeight: 'bold'
+},
+rowCard: {
+	height: 50,
+	borderWidth: 2,
+	borderBottomLeftRadius: 10,
+	borderTopRightRadius: 10,
+	margin: 5,
+	flexDirection: 'row',
+	alignItems: 'center',
+	justifyContent: 'space-around',	
 	
-  });
+},
+
+});
